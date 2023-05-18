922a90d53b06a3e6e9fdbda214eb75d8d608510a
Karina <karinaalmeida523@gmail.com>
2023-05-18 16:25:00 -0300

    redis running on events

--- src/repositories/event-repository/index.ts
+++ src/repositories/event-repository/index.ts
@@ -1,7 +1,21 @@
-import { prisma } from '@/config';
+import { Event } from '.prisma/client';
+import { prisma, redis } from '@/config';
 
-async function findFirst() {
-  return prisma.event.findFirst();
+async function findFirst(): Promise<Event> {
+  const cacheKey = 'event_data';
+  const cachedEvent = await redis.get(cacheKey);
+
+  if (cachedEvent) {
+    console.log('cache');
+    const event = JSON.parse(cachedEvent);
+    return event;
+  }
+
+  const event = await prisma.event.findFirst();
+
+  redis.set(cacheKey, JSON.stringify(event));
+
+  return event;
 }
 
 const eventRepository = {
