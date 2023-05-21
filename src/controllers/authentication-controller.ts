f535489a18c154b36f1c7a30ac65fe63d6012f4b
Jefferson Araújo <jeffersonc201212@gmail.com>
2023-05-20 23:53:26 -0300

    Feat: OAuth github has been implemented

--- src/controllers/authentication-controller.ts
+++ src/controllers/authentication-controller.ts
@@ -1,6 +1,12 @@
 import { Request, Response } from 'express';
 import httpStatus from 'http-status';
+import axios from 'axios';
+import jwt from 'jsonwebtoken';
 import authenticationService, { SignInParams } from '@/services/authentication-service';
+import userService from '@/services/users-service';
+import generateRandomString from '@/utils/randomString';
+import userRepository from '@/repositories/user-repository';
+import sessionRepository from '@/repositories/session-repository';
 
 export async function singInPost(req: Request, res: Response) {
   const { email, password } = req.body as SignInParams;
@@ -11,5 +17,19 @@ export async function singInPost(req: Request, res: Response) {
     return res.status(httpStatus.OK).send(result);
   } catch (error) {
     return res.status(httpStatus.UNAUTHORIZED).send({});
+  }
+}
+
+export async function signInOAuthGithubPost(req: Request, res: Response) {
+  const { code } = req.body;
+
+  try {
+    const session = await authenticationService.signInWithOAuthGithub(code);
+    return res.send(session);
+  } catch (error) {
+    if (error.name === 'UnauthorizedError') {
+      return res.status(httpStatus.UNAUTHORIZED).send(error.message);
+    }
+    return res.status(500).send(error.message);
   }
 }
--- src/routers/authentication-router.ts
+++ src/routers/authentication-router.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
-import { singInPost } from '@/controllers';
+import { signInOAuthGithubPost, singInPost } from '@/controllers';
 import { validateBody } from '@/middlewares';
-import { signInSchema } from '@/schemas';
+import { signInSchema, signInOAuthGithubSchema } from '@/schemas';
 
 const authenticationRouter = Router();
 
 authenticationRouter.post('/sign-in', validateBody(signInSchema), singInPost);
+authenticationRouter.post('/sign-in/oauth/github', validateBody(signInOAuthGithubSchema), signInOAuthGithubPost);
 
 export { authenticationRouter };
